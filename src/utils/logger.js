b5eeed65f3413e376547a8187387189c71a59e6c
Aakash Mansukhani <aakashmansukhani@Aakashs-MacBook-Pro.local>
2019-06-09 20:32:46 -0400

    Formatting js for logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,14 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-    format: format.combine(
-        format.timestamp(),
-        format.json(),
-        format.printf(info => `${JSON.stringify({level: info.level, message: info.message, requestParameters: info.requestParameters, timestamp: info.timestamp})}`)
-      ),
-      silent: process.env.NODE_ENV === 'test',
-    transports: [
-      new transports.Console()
-    ]
-  });
+  format: format.combine(
+      format.timestamp(),
+      format.json(),
+      format.printf(info => `${JSON.stringify({level: info.level, message: info.message, requestParameters: info.requestParameters, timestamp: info.timestamp})}`)
+    ),
+    silent: process.env.NODE_ENV === 'test',
+  transports: [
+    new transports.Console()
+  ]
+});
 module.exports = logger
